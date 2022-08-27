30ef074d114687520fb1810c73dd6692d7faf773
8ecomeDreamer <1412415464@qq.com>
2022-08-27 09:23:49 +0800

    sec

---
+++ src/screen/project-list/SearchPanel.jsx
@@ -0,0 +1,30 @@
+import userEvent from "@testing-library/user-event"
+import { useEffect } from "react"
+
+export const SearchPanel = (params,setParams) => {
+    
+
+    return <form action=''>
+        {/* input 输入框 */}
+        <input type="text"  value={params.name} onChange={(event)=>{
+            // 修改变量
+            setParams({
+                // 解构
+                ...params,
+                // 修改name属性
+                name: event.target.value
+            })
+        }}/>
+        {/* select 选择框 */}
+        <select>
+            {/* 列表渲染 */}
+            {
+                users.map(item => {
+                    return <option key={item.id} value={item.id}>{item.name}</option>
+                })
+            }
+            
+        </select>
+        
+    </form>
+}
---
+++ src/screen/project-list/index.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+
+export const ProjectListScreen = () =>{
+     // 初始化用户变量 数组
+     const [users,setUsers]=useState([])
+     // 初始化参数变量 对象
+     const [params,setParams]=useState({
+         name:"",
+         personId:""
+     })
+     const [List,setList]=useState([])
+ 
+ 
+     // 逻辑
+     useEffect(()=>{
+         // 发送网络请求
+         fetch('').then(async(response) =>{
+             if(response.ok ){
+                 // setList(await response.json())
+             }
+         })
+     },[])
+ 
+    return <div>
+        {/* props 传递变量 */}
+        {/* 主页有搜索框 展示数据的列表  搜索框需要通过props传递参数*/}
+        <SearchPanel params={params} setParams={setParams} ></SearchPanel>
+        <List  List={List}>负责人</List>
+    </div>
+}
